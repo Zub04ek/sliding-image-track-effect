9b8690e4a6dd7250b9500f68e55bcda272f94075
Nastya <nastasyz@ukr.net>
2024-01-30 17:03:53 +0200

    added base url in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sliding-image-track-effect/",
   plugins: [
     vue(),
     vueJsx(),
